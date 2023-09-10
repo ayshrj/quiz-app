eca5c6ea8e63922c7c80d1d54f98bb4e32c26702
Ayush <ayshrj10@gmail.com>
2023-09-11 02:07:53 +0530

    Console log data, connection left

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import { createRequire } from "module"; //incase need to use require
+import { createRequire } from "module"; // in case you need to use require
 const require = createRequire(import.meta.url);
 
 const express = require("express");
@@ -11,10 +11,54 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+import { config } from "dotenv";
+config();
+
+import { Configuration, OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
+
 app.post("/search", (req, res) => {
-  const searchData = req.body.query;
-  // Process searchData here
-  res.json({ searchData });
+  const Topic = req.body.query;
+  const Basic = `Give a quiz with 2 questions on ${Topic} in the JSON format like
+  Eg:
+  [
+    {
+      "question": "Question Here",
+      "options": [
+        "Option A",
+        "Option B",
+        "Option C",
+        "Option D"
+      ],
+      "correct": "2"
+    }
+  ]
+  
+  Note: correct has 0-based indexing`;
+
+  let data = null;
+
+  openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: Basic }],
+    })
+    .then((response) => {
+      data = response.data.choices[0].message.content;
+      console.log(response.data.choices[0].message.content);
+      res.json({ data });
+    })
+    .catch((error) => {
+      console.error("OpenAI API error:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing your request" });
+    });
 });
 
 app.listen(port, () => {
--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./SearchBox.css";
 
 export const SearchBox = () => {
     const [notes, setNotes] = useState([]);
-
     const [query, setQuery] = useState('');
-
     const [result, setResult] = useState('');
 
     const searchTopic = async (event) => {
         event.preventDefault();
-        // console.log('button clicked', event.target);
+        console.log('button clicked');
         try {
           const res = await axios.post('http://localhost:5000/search', { query });
-          setResult(res.data.searchData);
-          console.log(result);
+          setResult(res.data.data);
         } catch (error) {
           console.error('Error:', error);
         }
     }
 
     const inputChange = (event) => {
-        // console.log(event.target.value);
         setQuery(event.target.value);
     }
+
+    useEffect(() => {
+        console.log(result);
+    }, [result]);
 
     return (
         <div>
